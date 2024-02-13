30ba5ef8ed89b3e0e6b38a7ad16cdd8b633d420e
Kavin Charles <87717754+Kavin-Charles@users.noreply.github.com>
2024-02-13 20:20:22 +0530

    Create Celsius to Fahrenheit.c

---
+++ Celsius to Fahrenheit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+void main()
+{
+    float c,f;
+    clrscr();
+ 
+    printf("\n Enter the Temparature in Celsius : ");
+    scanf("%f",&c);
+ 
+    f = (1.8 * celsius) + 32;
+    printf("\n Temperature in Fahrenheit : %f ",f);
+ 
+    getch();
+}
