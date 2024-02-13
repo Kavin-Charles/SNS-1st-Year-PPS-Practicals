6af9c95108c5e5c32ada0dd25821123a96b93dde
Kavin Charles <87717754+Kavin-Charles@users.noreply.github.com>
2024-02-13 20:16:54 +0530

    Create Swapping Without Third Var.c

---
+++ Swapping Without Third Var.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int x = 10, y = 5;
+    clrscr();
+
+    printf("After Swapping: x = %d, y = %d", x, y);
+
+    x = x + y; // x now becomes 15
+    y = x - y; // y becomes 10
+    x = x - y; // x becomes 5
+ 
+    printf("After Swapping: x = %d, y = %d", x, y);
+    getch();
+    return 0;
+}
